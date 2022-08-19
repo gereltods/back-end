36b23df9c66ffda1f0c3bdfe87f4d197aa0cc173
Gerelt-Od <oju-mac@oju-mac.local>
2022-08-19 09:17:59 +0800

    test change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.APP_PORT || 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello AWS Apprunner!')
 })
 
 app.listen(port, () => {
